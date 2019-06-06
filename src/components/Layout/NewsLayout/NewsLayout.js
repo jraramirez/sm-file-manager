d0a5acd716b6233e6281a773ccda43c696d1a91c
unknown <mirr@digiters.co>
2019-06-06 23:03:58 +0800

    news styles fixes

--- src/components/Layout/NewsLayout/NewsLayout.js
+++ src/components/Layout/NewsLayout/NewsLayout.js
@@ -53,8 +53,86 @@ const NewsLayout = (props) => (
           <Link to="/">Mootral blog</Link>
         </Col>
       </Link>
+      <Link className={classes.ThumbLink} to="/">
+        <Col className={classes.ImgThumb} md={3}>
+          <div>
+            <img alt="thumb1" src={thumbImg1} />
+          </div>
+        </Col>
+        <Col md={9}>
+          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <Link to="/">Mootral blog</Link>
+        </Col>
+      </Link>
+      <div className={classes.Divider}>
+      </div>
 
-      <button className={classes.btnGreen}>See more</button>
+      <Link className={classes.ThumbLink} to="/">
+        <Col className={classes.ImgThumb} md={3}>
+          <div>
+            <img alt="thumb2" src={thumbImg2} />
+          </div>
+        </Col>
+        <Col md={9}>
+          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <Link to="/">Mootral blog</Link>
+        </Col>
+      </Link>
+      <div className={classes.Divider}>
+      </div>
+
+      <Link className={classes.ThumbLink} to="/">
+        <Col className={classes.ImgThumb} md={3}>
+          <div>
+            <img alt="thumb3" src={thumbImg3} />
+          </div>
+        </Col>
+        <Col md={9}>
+          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <Link to="/">Mootral blog</Link>
+        </Col>
+      </Link>
+      <Link className={classes.ThumbLink} to="/">
+        <Col className={classes.ImgThumb} md={3}>
+          <div>
+            <img alt="thumb1" src={thumbImg1} />
+          </div>
+        </Col>
+        <Col md={9}>
+          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <Link to="/">Mootral blog</Link>
+        </Col>
+      </Link>
+      <div className={classes.Divider}>
+      </div>
+
+      <Link className={classes.ThumbLink} to="/">
+        <Col className={classes.ImgThumb} md={3}>
+          <div>
+            <img alt="thumb2" src={thumbImg2} />
+          </div>
+        </Col>
+        <Col md={9}>
+          <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <Link to="/">Mootral blog</Link>
+        </Col>
+      </Link>
+      <div className={classes.Divider}>
+      </div>
+      <Col md={12}>
+        <button className={classes.btnGreen}>See more</button>
+      </Col>
+      <Col md={12}>
+        <div className={classes.Pagination}>
+          <span className={classes.ActivePage}>1</span> 2 3 4 5
+        </div>
+      </Col>
+
     </Row>
   </div>
 );
